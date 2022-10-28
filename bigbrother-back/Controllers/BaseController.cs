94422a039e9225c05b7dda4bf2b569d44029de72
PumaMargo <margaret.kyselgova@gmail.com>
2022-10-28 20:48:49 +0300

    add base controller

---
+++ bigbrother-back/Controllers/BaseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+
+namespace bigbrother_back.Utility
+{
+    [ApiController]
+    [Produces(MediaTypeNames.Application.Json)]
+    [Route("api/[controller]")]
+    public class BaseController : ControllerBase
+    {
+        public const int MaxPageCount = 2000;
+    }
+}
