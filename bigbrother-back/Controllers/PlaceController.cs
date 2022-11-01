f452ccc43f5ef823f8906c038a4a9748820fb56f
PumaMargo <margaret.kyselgova@gmail.com>
2022-11-01 22:42:37 +0200

    add Place controller

---
+++ bigbrother-back/Controllers/PlaceController.cs
@@ -0,0 +1,95 @@
+﻿using bigbrother_back.DataContext;
+using bigbrother_back.Models.Api;
+using bigbrother_back.Models.DataModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Net.Mime;
+
+namespace bigbrother_back.Controllers
+{
+    public class PlaceController : BaseDataContextController
+    {
+        #region Construction
+
+        public PlaceController(DatabaseContext dbContext, ILogger<DebugController> logger)
+            : base(dbContext, logger)
+        {
+        }
+
+        #endregion
+
+        #region Endpoints
+
+        [HttpGet("List")]
+        [Authorize(Roles = $"{nameof(AccountRole.Administrator)}, {nameof(AccountRole.Manager)}")]
+        public async Task<ActionResult<IEnumerable<PlaceResponce>>> GetListAsync([Range(1, MaxPageCount)] int? limit,
+                                                                                 [Range(0, int.MaxValue)] int? offset)
+        {
+            var places = await DataModel.Places.OrderBy(p => p.Id)
+                                               .Skip(offset ?? 0)
+                                               .Take(limit ?? MaxPageCount)
+                                               .ToListAsync();
+
+            var res = places.Select(p => new PlaceResponce()
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Description = p.Description,
+            });
+
+            return Ok(res);
+        }
+
+        [HttpGet("{id:int}")]
+        [Authorize(Roles = $"{nameof(AccountRole.Administrator)}, {nameof(AccountRole.Manager)}")]
+        public async Task<ActionResult<PlaceResponce>> GetAsync([Range(1, int.MaxValue)] int id)
+        {
+            var place = await DataModel.Places.FirstOrDefaultAsync(p => p.Id == id);
+            if (place == null)
+            {
+                return Problem("Place not found", null, StatusCodes.Status404NotFound);
+            }
+
+            var res = new PlaceResponce()
+            {
+                Id = place.Id,
+                Name = place.Name,
+                Description = place.Description,
+            };
+
+            return Ok(res);
+        }
+
+        [HttpDelete("{id:int}")]
+        [Authorize(Roles = $"{nameof(AccountRole.Administrator)}, {nameof(AccountRole.Manager)}")]
+        public async Task<ActionResult> DeleteAsync([Range(1, int.MaxValue)] int id)
+        {
+            var place = await DataModel.Places.FirstOrDefaultAsync(p => p.Id == id);
+            if (place == null)
+            {
+                return Problem("Marker not found", null, StatusCodes.Status404NotFound);
+            }
+
+            DataModel.Places.Remove(place);
+            await DataModel.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpPost()]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [Authorize(Roles = $"{nameof(AccountRole.Administrator)}, {nameof(AccountRole.Manager)}")]
+        public async Task<ActionResult> CreateAsync(CreatePlaceRequest place)
+        {
+            DataModel.Places.Add(new Place() { Name = place.Name, Description = place.Description });
+            await DataModel.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        #endregion
+    }
+}
---
+++ bigbrother-back/Models/Api/Place.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace bigbrother_back.Models.Api
+{
+    public class CreatePlaceRequest
+    {
+        [MaxLength(256)]
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+    }
+
+    public class PlaceResponce
+    {
+        public int Id { get; set; }
+
+        [MaxLength(256)]
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+    }
+}
